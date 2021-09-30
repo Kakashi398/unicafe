32fafc9c516040ab03c3de36d31953884161d405
kakashi398 <tonymalic398@gmail.com>
2021-09-30 15:51:21 +0000

    1.9: unicafe step4

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import { useState } from "react";
 
 const Statistics = ({good, neutral, bad}) =>{
+
   
   const all = good + neutral +bad
   const average = all / 3
   const positive = (good + neutral) / 3 * 100
+
+  if(good === 0 && neutral ===0 && bad ===0){
+    return (
+      <div>
+        no feedback given
+      </div>
+    )
+  } 
+
 
   return(
     <div>
@@ -21,10 +31,30 @@ const Statistics = ({good, neutral, bad}) =>{
   )
 }
 
+
+
+
+
 const App =() => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+
+
+  const goodClick = () => {
+    setGood(good)
+    setGood(good + 1)
+  }
+
+  const neutralClick = () => {
+    setNeutral(neutral)
+    setNeutral(neutral + 1)
+  }
+
+  const badClick = () => {
+    setBad(bad)
+    setBad(bad + 1)
+  }
   
   
   
@@ -33,12 +63,11 @@ const App =() => {
       <h1>give feedback</h1>
       
       
-      <button onClick={() => setGood(good +1)} >good</button>
+      <button onClick={goodClick} >good</button>
 
-      <button onClick={() => setNeutral(neutral + 1)}>neutral</button>
+      <button onClick={neutralClick}>neutral</button>
 
-      <button onClick={() => setBad(bad + 1)}>bad</button>
-
+      <button onClick={badClick}>bad</button>
 
     < Statistics good={good} neutral={neutral} bad={bad} />
     </div>  
